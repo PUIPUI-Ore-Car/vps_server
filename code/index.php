b6ed901607e8365c7afd7cca9e698d910aeaea8d
kazuki19992 <kushida98@gmail.com>
2021-07-06 13:21:00 +0900

    init

---
+++ code/index.php
@@ -0,0 +1,27 @@
+<?php
+  // 文字コード設定
+  header('Content-Type: application/json; charset=UTF-8');
+
+  switch(strtolower($_SERVER['REQUEST_METHOD'])){
+    case 'get': {
+      // データを取得する
+
+
+      $json = json_encode( array('method' => strtolower($_SERVER['REQUEST_METHOD']), 'keys' => 3) );
+      echo $json;
+    }
+    case 'post': {
+      // データを受信する
+
+      $postData = json_decode(file_get_contents('php://input'));
+      $postData["method"] = "post";
+      // var_dump($postData);
+      $json = json_encode( $postData );
+      echo $json;
+
+    }
+    default: {
+      // エラー
+
+    }
+  }
