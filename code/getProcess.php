c740ad3d7b4e6c45a8cc10de0074dbcda46228cf
kazuki19992 <kushida98@gmail.com>
2021-08-17 16:25:26 +0900

    a

--- code/getProcess.php
+++ code/getProcess.php
@@ -19,7 +19,7 @@
     $tempData = getAllTemp(get_db_connect());
     $network = getJson('./network.json');
 
-    $data = array('method' => strtolower($_SERVER['REQUEST_METHOD']), 'command' => 'AllData','odo' => $odo, 'temp' => $tempData["temp"], 'humidity' => $tempData["humidity"], 'ssid' => $network["ssid"], 'ipAddr' => $network["ipAddr"], 'time' => $tempData["time"]);
+    $data = array('method' => strtolower($_SERVER['REQUEST_METHOD']), 'command' => 'AllDataFetch','odo' => $odo, 'temp' => $tempData["temp"], 'humidity' => $tempData["humidity"], 'ssid' => $network["ssid"], 'ipAddr' => $network["ipAddr"], 'time' => $tempData["time"]);
 
     $json = json_encode( $data );
     echo $json;
